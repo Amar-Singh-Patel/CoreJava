f9c70e15c10f6fa37a6c249ddb92f5116e41a33b
amar <amar@DESKTOP-N0NB5CF>
2019-06-24 23:44:47 +0530

    second

---
+++ MavenApp/src/main/java/com/sk/test/App.java
@@ -0,0 +1,13 @@
+package com.sk.test;
+
+
+public class App 
+{
+	public static void say(String name) {
+		System.out.println("hello "+name);
+	}
+    public static void main( String[] args )
+    {
+      say("sandeep");
+    }
+}
